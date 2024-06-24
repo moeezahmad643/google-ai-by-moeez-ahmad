8cc3e2e9cc03a082e053b8c3071bc7e5db5b3c82
Moeez Ahmad <moeezahmad.pk@gmail.com>
2024-06-24 02:28:29 -0700

    with /

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const genAI = new GoogleGenerativeAI(process.env.MY_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const app = express();
 
+app.get('/',(req,res)=>{
+  res.send('Server is Live');
+})
+
 app.get("/output", async (req, res) => {
   const prompt = req.query.prompt;
   const result = await model.generateContent(prompt);
